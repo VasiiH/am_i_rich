8e205aa6d3971decae9085d14be008daeb2e51b2
Vasi <https://github.com/VasiiH/am_i_rich>
2020-10-26 11:11:54 +0530

    Am I Rich application using flutter

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor:
+            Colors.blueGrey.shade600, // Color is a Child of scaffold Widget
+        appBar: AppBar(
+          // AppBar is a Child of scaffold Widget
+          title: Text("Am I Rich!"), // Child of AppBar Widget
+          backgroundColor: Colors.blueGrey.shade900, //Child of AppBar Widget
+        ),
+        body: Center(
+          // child of Scaffold Widget
+          child: Image(
+            //child of Center Widget
+            image: AssetImage(// child of Image Widget
+                'images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
